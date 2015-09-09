d5e0cd7652778854b2e376cb8f7d8b8be17a26ee
romancortes <javier.roman.c@gmail.com>
2015-09-09 17:19:12 +0200

    Add Calendar

---
+++ ui/calendar-widget-day.reel/calendar-widget-day.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/calendar-widget-day.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CalendarWidgetDay
+ * @extends Component
+ */
+exports.CalendarWidgetDay = Component.specialize(/** @lends CalendarWidgetDay# */ {
+    constructor: {
+        value: function CalendarWidgetDay() {
+            this.super();
+        }
+    }
+});
