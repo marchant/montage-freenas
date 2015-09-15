582812d9bde6087931a4aa685f0a689df09c0e34
Thibault Zanini <thibaultzanini@gmail.com>
2015-09-15 09:23:21 -0700

    Populates System Information of the hardware part.

--- ui/hardware/hardware.reel/hardware.js
+++ ui/hardware/hardware.reel/hardware.js
@@ -4,4 +4,28 @@ var Component = require("montage/ui/component").Component;
  * @class Hardware
  * @extends Component
  */
-exports.Hardware = Component.specialize({});
+exports.Hardware = Component.specialize({
+
+    enterDocument: {
+        value: function (firstTime) {
+            if (firstTime) {
+                this.populateSystemInfo();
+            }
+        }
+    },
+
+    system: {
+        value: null
+    },
+
+    populateSystemInfo: {
+        value: function () {
+            var self = this;
+
+            this.application.controller.getSystemInfo().then(function (_system) {
+                self.system = _system;
+            });
+        }
+    }
+
+});
