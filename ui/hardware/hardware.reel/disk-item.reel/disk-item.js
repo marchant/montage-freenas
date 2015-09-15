f8c35a25b9c7b454cbe8a4bf0d6caa569816568d
joshuarule <joshua@joshuarule.com>
2015-09-14 23:37:30 -0400

    hadware page layout and styles

---
+++ ui/hardware/hardware.reel/disk-item.reel/disk-item.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/disk-item.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class DiskItem
+ * @extends Component
+ */
+exports.DiskItem = Component.specialize(/** @lends DiskItem# */ {
+    constructor: {
+        value: function DiskItem() {
+            this.super();
+        }
+    }
+});
